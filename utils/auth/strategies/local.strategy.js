02e38ac8cbb2e1393f9df40c82dc8e383186d0ed
MARIANOCEREDA <gmarianocereda@gmail.com>
2022-03-26 22:35:16 +0100

    Implementacion de "local strategy" para autenticacion

--- utils/auth/strategies/local.strategy.js
+++ utils/auth/strategies/local.strategy.js
@@ -1,3 +1,27 @@
-const { Strategy } = require('passport-local');
+/*
+*@description : Module that create the instance and logic from the auth strategy.
+*
+*/
 
-const LocalStrategy =  new Strategy();
+const { Strategy } = require('passport-local');
+const bcrypt = require('bcrypt');
+const  {UserService}  = require('../../../services/user.service');
+const service = new UserService();
+
+const LocalStrategy =  new Strategy(async (email,password,done)=>{
+  try{
+    const user = await service.findByEmail(email);
+    if(!user){
+      done(boom.unauthorized(),false);
+    }
+    const isMatch = await bcrypt.compare(password,user.password);
+    if(!isMatch){
+      done(boom.unauthorized(),false);
+    }
+    done( null,user);//Si todo es autorizado, esta linea se ejecuta
+  }catch(error){
+    done(error,false);
+  }
+});
+
+module.exports = { LocalStrategy }
