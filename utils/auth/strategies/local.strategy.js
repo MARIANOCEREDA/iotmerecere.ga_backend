7d3429084fae08a2916c83d790f14fa8bad0a907
MARIANOCEREDA <gmarianocereda@gmail.com>
2022-03-26 23:20:42 +0100

    Correcciones pequeñas y agregado de comentarios

--- utils/auth/strategies/local.strategy.js
+++ utils/auth/strategies/local.strategy.js
@@ -1,14 +1,21 @@
-/*
+/**
 *@description : Module that create the instance and logic from the auth strategy.
-*
+*@requires passport-local -> contiene el objeto para la estrategia de comprobacion
+*@requires boom -> error HANDLER
+*@requires UserService -> servicios de usurio.
 */
-
 const { Strategy } = require('passport-local');
 const bcrypt = require('bcrypt');
-const  {UserService}  = require('../../../services/user.service');
+const boom = require('@hapi/boom');
+const { UserService }  = require('../../../services/user.service');
 const service = new UserService();
 
-const LocalStrategy =  new Strategy(async (email,password,done)=>{
+const LocalStrategy =  new Strategy(
+  {
+    usernameField: 'email',
+    passwordField: 'password'
+  },
+  async (email,password,done)=>{
   try{
     const user = await service.findByEmail(email);
     if(!user){
@@ -18,10 +25,11 @@ const LocalStrategy =  new Strategy(async (email,password,done)=>{
     if(!isMatch){
       done(boom.unauthorized(),false);
     }
+    delete user.dataValues.password;
     done( null,user);//Si todo es autorizado, esta linea se ejecuta
   }catch(error){
     done(error,false);
   }
 });
 
-module.exports = { LocalStrategy }
+module.exports =  LocalStrategy;
