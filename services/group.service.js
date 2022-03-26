f97e6b7f47bfe09b481a5739edee40c1305f3a79
MARIANOCEREDA <gmarianocereda@gmail.com>
2022-03-26 19:54:02 +0100

    Creacion de findOne y agregado de "user"

--- services/group.service.js
+++ services/group.service.js
@@ -6,6 +6,28 @@ class GroupService{
     const newGroup = await models.Group.create(group);
     return newGroup;
   }
+
+  async findAll(){
+    const allGroups = await models.Group.findAll({
+      include:['user']
+    });
+    if(!allGroups){
+      throw boom.notFound("No devices were found");
+    };
+    allGroups.forEach((element,item, i) => {
+      delete element.user.dataValues.password;
+    });
+    return allGroups;
+  }
+
+  async findOne(id){
+    const group = await models.Group.findByPk(id,{
+      include:'user'
+    });
+    delete group.user.dataValues.password;
+    return group
+  }
+
 }
 
 module.exports = { GroupService }
