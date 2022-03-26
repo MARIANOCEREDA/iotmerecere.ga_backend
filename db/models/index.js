79dfdfb1302132ea8ff1a4d31430653e12527f1d
MARIANOCEREDA <gmarianocereda@gmail.com>
2022-03-26 20:05:49 +0100

    Eliminacion de DEVICEGROUP init y de DEVICEGROUP associate

--- db/models/index.js
+++ db/models/index.js
@@ -1,18 +1,15 @@
 const {User,UserSchema}=require('./user.model');
 const {Device,DeviceSchema}=require('./device.model');
 const {Group,GroupSchema}=require('./group.model');
-const {DeviceGroup,DeviceGroupSchema}=require('./device_group.model');
 
 function setUpModels(sequelize){
   User.init(UserSchema, User.config(sequelize));
   Device.init(DeviceSchema,Device.config(sequelize));
   Group.init(GroupSchema,Group.config(sequelize));
-  DeviceGroup.init(DeviceGroupSchema,DeviceGroup.config(sequelize));
 
   User.associate(sequelize.models);
   Device.associate(sequelize.models);
-  Group.associate(sequelize.models)
-  DeviceGroup.associate(sequelize.models);
+  Group.associate(sequelize.models);
 }
 
 module.exports = {setUpModels}
